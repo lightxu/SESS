2a75bdaada558cbcaddf76644d62d4c515040a3e
Chenwei Xie <weiweichenxie@gmail.com>
2014-06-19 21:16:25 +0800

    add test of mysql

---
+++ test/natural.sql
@@ -0,0 +1,5 @@
+insert into natural_customer values('P1403178658', 'ChenweiXie', '339005199307240035', 2014-06-19, '男','浙江大学','学生','本科','浙江大学', '18868819014','1','Laohe Branch', '', 0);
+insert into natural_customer values('P1403178659', 'ChenshiXue', '339005199307240036', 2014-06-19, '男','浙江大学','学生','本科','浙江大学', '18868819015','1','Laohe Branch', '', 0);
+insert into natural_customer values('P1403178660', 'Mingwei', '339005199307240037', 2014-06-19, '男','浙江大学','学生','本科','浙江大学', '18868819016','1','Laohe Branch', '', 1);
+insert into natural_customer values('P1403178661', 'LingZhan', '339005199307240038', 2014-06-19, '男','浙江大学','学生','本科','浙江大学', '18868819017','1','Laohe Branch', '', 0);
+insert into natural_customer values('P1403178662', 'LonghuJin', '339005199307240039', 2014-06-19, '男','浙江大学','学生','本科','浙江大学', '18868819018','1','Laohe Branch', '', 0);
---
+++ test/personnel.sql
@@ -0,0 +1,5 @@
+insert into personnel values('339005199307240030', 'lightxu');
+insert into personnel values('339005199307240031', 'ChenweiXie');
+insert into personnel values('339005199307240032', 'LinZhang');
+insert into personnel values('339005199307240033', 'MingWei');
+insert into personnel values('339005199307240034', 'LonghuJin');
